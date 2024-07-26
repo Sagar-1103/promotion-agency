f99476251fa43f9b265a2e9436e6d9a2689bb86d
Sagar Shirgaonkar <sagarshirgaonkar59511@gmail.com>
2024-07-27 00:15:47 +0530

    minor changes

--- components/Background.tsx
+++ components/Background.tsx
@@ -7,10 +7,10 @@ export function BackgroundBeamsDemo() {
   return (
     <div className="h-[40rem] w-full dark:bg-neutral-950 bg-slate-50 relative flex flex-col items-center justify-center antialiased">
       <div className="max-w-3xl mx-auto p-4 text-center">
-        <h1 className="relative z-10 text-lg py-3 md:text-7xl bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-600 font-sans font-bold">
+        <h1 className="relative z-10 text-5xl py-3 md:text-7xl bg-clip-text text-gray-700 dark:text-gray-300  font-sans font-bold">
           Take Your YouTube Channel to New Heights
         </h1>
-        <p className="text-neutral-500 max-w-lg mx-auto my-8 text-sm relative z-10">
+        <p className="text-neutral-700 dark:text-neutral-300 max-w-lg mx-auto my-8 text-sm relative z-10">
           Ready to amplify your YouTube presence? With our expertise in channel optimization, SEO, and content creation, we can help you attract more viewers, engage your audience, and grow your brand. Let’s unlock your channel’s full potential together.
         </p>
         
--- components/Header.tsx
+++ components/Header.tsx
@@ -39,14 +39,13 @@ const Navbar = () => {
 
   return (
     <nav
-    style={{
-      background: 'linear-gradient(271.1deg, rgba(169, 204, 236, 0.2) 37.05%, rgba(90, 125, 154, 0.13) 66.11%)',
-      boxShadow: 'rgba(0, 0, 0, 0.25) 2px 2px 12px 2px',
-      backdropFilter: 'blur(3.5px)',
-    }}
-    className="fixed top-4 left-4 right-4 bg-transparent shadow-lg rounded-lg backdrop-filter backdrop-blur-2xl z-50"
-  >
-  
+      style={{
+        background: 'linear-gradient(271.1deg, rgba(169, 204, 236, 0.2) 37.05%, rgba(90, 125, 154, 0.13) 66.11%)',
+        boxShadow: 'rgba(0, 0, 0, 0.25) 2px 2px 12px 2px',
+        backdropFilter: 'blur(3.5px)',
+      }}
+      className="fixed top-4 left-4 right-4 bg-transparent shadow-lg rounded-lg backdrop-filter backdrop-blur-2xl z-50"
+    >
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
@@ -58,36 +57,53 @@ const Navbar = () => {
           </div>
           <div className="hidden md:flex space-x-8 items-center">
             <Link href="/services">
-              <p className=" text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
+              <p className="text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
                 Services
               </p>
             </Link>
             <Link href="/pricing">
-              <p className=" text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
+              <p className="text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
                 Pricing
               </p>
             </Link>
             <Link href="/about">
-              <p className=" text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
+              <p className="text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
                 About Us
               </p>
             </Link>
             <Link href="/contact">
-              <p className=" text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
+              <p className="text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150">
                 Contact
               </p>
             </Link>
             <button
               onClick={toggleDarkMode}
-              className=" text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150"
+              className="text-gray-900 dark:text-gray-200 hover:text-teal-500 transition duration-150"
             >
               {darkMode ? <Sun className="h-6 w-6" /> : <Moon className="h-6 w-6" />}
             </button>
           </div>
-          <div className="flex items-center md:hidden">
+          <div className="flex space-x-5 items-center md:hidden">
+          <motion.div
+                initial={{ opacity: 0, y: -10 }}
+                animate={{ opacity: 1, y: 0 }}
+                exit={{ opacity: 0, y: -10 }}
+                transition={{
+                  delay: 0.4, // Additional delay for the dark mode toggle button
+                  duration: 0.3,
+                  ease: "easeInOut"
+                }}
+              >
+                <button
+                  onClick={toggleDarkMode}
+                  className="block text-black md:hidden dark:text-white hover:text-teal-500 transition duration-150 px-3 py-2 rounded-md text-base font-medium"
+                >
+                  {darkMode ? <Sun className="h-6 w-6" /> : <Moon className="h-6 w-6" />}
+                </button>
+              </motion.div>
             <button
               onClick={toggleMenu}
-              className="text-white dark:text-white hover:text-teal-500 transition duration-150"
+              className="text-black dark:text-white hover:text-teal-500 transition duration-150"
             >
               {isOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
             </button>
@@ -98,11 +114,16 @@ const Navbar = () => {
       <AnimatePresence>
         {isOpen && (
           <motion.div
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0, y: -20 }}
-            transition={{ duration: 0.4, ease: "easeOut" }}
-            className="md:hidden bg-gray-700 bg-opacity-80 dark:bg-gray-800 dark:bg-opacity-80 backdrop-filter backdrop-blur-lg"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: 'auto' }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            style={{
+              background: 'linear-gradient(271.1deg, rgba(169, 204, 236, 0.2) 37.05%, rgba(90, 125, 154, 0.13) 66.11%)',
+              boxShadow: 'rgba(0, 0, 0, 0.25) 2px 2px 12px 2px',
+              backdropFilter: 'blur(3.5px)',
+            }}
+            className="md:hidden  bg-transparent overflow-hidden"
           >
             <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
               {['Services', 'Pricing', 'About Us', 'Contact'].map((text, index) => (
@@ -114,33 +135,17 @@ const Navbar = () => {
                   transition={{
                     delay: index * 0.1, // Stagger delay
                     duration: 0.3,
-                    ease: "easeOut"
+                    ease: "easeInOut"
                   }}
                 >
                   <Link href={`/${text.toLowerCase().replace(/\s+/g, '-')}`}>
-                    <p className="block text-white dark:text-white hover:text-teal-500 transition duration-150 px-3 py-2 rounded-md text-base font-medium">
+                    <p className="block text-black text-center dark:text-white hover:text-teal-500 transition duration-150 px-3 py-2 rounded-md text-base font-medium">
                       {text}
                     </p>
                   </Link>
                 </motion.div>
               ))}
-              <motion.div
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                transition={{
-                  delay: 0.4, // Additional delay for the dark mode toggle button
-                  duration: 0.3,
-                  ease: "easeOut"
-                }}
-              >
-                <button
-                  onClick={toggleDarkMode}
-                  className="block text-white dark:text-white hover:text-teal-500 transition duration-150 px-3 py-2 rounded-md text-base font-medium"
-                >
-                  {darkMode ? <Sun className="h-6 w-6" /> : <Moon className="h-6 w-6" />}
-                </button>
-              </motion.div>
+              
             </div>
           </motion.div>
         )}
